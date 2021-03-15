36da77aabaf78fb50cb1a2eb9a259c76125f563b
Vssblt <sunrunyangcp01@163.com>
2021-03-15 16:08:18 +0800

    add shell

---
+++ get-depends.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -A map
+
+function isExist()
+{
+	for key in ${!map[*]};do
+		if [ $key == $1 ]; then
+			echo 1
+			return 
+		fi
+	done
+	echo 0
+}
+
+logfile=./log
+function getDepends()
+{
+	ret=`apt-cache depends $1 | grep 依赖 | cut -d : -f2 | tr -d "<>"`
+	for line in $ret
+	do
+		if [ "0" == `isExist $line` ]; then
+			echo $line >> $logfile
+			map[$line]=$line
+			getDepends $line
+		fi
+	done
+}
+
+
+getDepends gcc
+getDepends build-essential
+getDepends make
+
+
